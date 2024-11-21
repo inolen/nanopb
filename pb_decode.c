976ba3789076f59e1d91fc5416478b1da681d56e
Anthony Pesch <inolen@gmail.com>
2024-11-20 21:05:37 -0500

    wip avoid conditional branch in pb_decode_svarint

--- pb_decode.c
+++ pb_decode.c
@@ -1447,13 +1447,11 @@ bool pb_decode_bool(pb_istream_t *stream, bool *dest)
 bool pb_decode_svarint(pb_istream_t *stream, pb_int64_t *dest)
 {
     pb_uint64_t value;
+
     if (!pb_decode_varint(stream, &value))
         return false;
-    
-    if (value & 1)
-        *dest = (pb_int64_t)(~(value >> 1));
-    else
-        *dest = (pb_int64_t)(value >> 1);
+
+    *dest = (pb_int64_t)(value >> 1) ^ -(pb_int64_t)(value & 0x1);
     
     return true;
 }
